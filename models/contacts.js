fd757ec0830c00b7a4f606bb9f8646b16271393f
OlenaIa <121794911+OlenaIa@users.noreply.github.com>
2023-09-26 19:44:10 +0300

    fix

--- models/contacts.js
+++ models/contacts.js
@@ -47,8 +47,8 @@ export const updateContact = async (contactId, body) => {
     return null;
   };
 
-  contacts[index] = { id: contactId, ...body };
-
+  contacts[index] = { ...contacts[index], ...body };
+  
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   return contacts[index];
 };
