ef3cc806f674389bbcaf75eac87c5d66f3ca7c25
OlenaIa <121794911+OlenaIa@users.noreply.github.com>
2023-10-10 19:56:27 +0300

    task favorite condition

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,9 +3,15 @@ import { Contact} from '../models/contact.js';
 
 export const getAll = async (req, res) => {
     const { _id: owner } = req.user;
-    const { page = 1, limit = 20 } = req.query;
+    const { page = 1, limit = 20, favorite = null } = req.query;
     const skip = (page - 1) * limit;
-    const result = await Contact.find({owner}, '-createdAt -updatedAt', {skip, limit}).populate('owner', 'email subscription');
+    const condition = { owner };
+    if (favorite !== null)  {
+        condition.favorite = favorite;
+    }
+    console.log(condition);
+    const result = await Contact.find(condition, '-createdAt -updatedAt', {skip, limit}).populate('owner', 'email subscription');
+    // .where({ favorite });
     res.status(200).json(result);
 };
 
